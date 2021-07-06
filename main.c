1696c4adc1ed5fbb01c246ee5f20830c36788e61
Vidy <contact.realvidy@gmail.com>
2021-07-06 15:49:22 +0200

    test: Testing pointer to NULL string

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 int main(int argc, char const *argv[])
 {
   char *myStr = "Ah que coucou";
+  char *nullStr = NULL;
 
   assert(ft_count(myStr, 'z') == 0);
   assert(ft_count(myStr, '\0') == 13);
   assert(ft_count(myStr, 'h') == 1);
   assert(ft_count(myStr, ' ') == 2);
   assert(ft_count(NULL, 'h') == 0);
+  assert(ft_count(nullStr, 'h') == 0);
 
   return 0;
 }
